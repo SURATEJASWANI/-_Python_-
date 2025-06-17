f42c17d30675925c867b4abb91a5e5d962660158
SURA TEJASWANI <sura.tejaswani.5@gmail.com>
2025-06-17 19:59:32 +0530

    Update NESTED LOOP.py

--- NESTED LOOP.py
+++ NESTED LOOP.py
@@ -2,10 +2,10 @@
 
 #1. print sets whose sum is 5
 
- for i in range(6):
-    for j in range(6):
-        if (i+j)==5:
-            print(i,j)
+for i in range(6):      #0              #1
+    for j in range(6):  #0,1,2,3,4,5     0,1,2,3,4,5
+        if (i+j)==5:    #0+5=True        1+4
+            print(i,j)  #0 5             1 4            2 3    3 2    4 1    5 0
 
 #2. print sets which are diviible by both 3 & 5
 
@@ -16,11 +16,18 @@ for i in range(6):
         elif (i%5==0) and (j%5==0):
             print(i,j)
 
-#3. print sets of range from 5-5 inner & outer loops
+#(or)-------------------------------(or)
 
 for i in range(6):
-     for j in range(6):
-          print(i,j)
+    for j in range(6):
+        if (i%3==0) and (j%3==0):
+            print(i,j)
+
+#3. print sets of range from 5-5 inner & outer loops
+
+for i in range(6):         #0                         1                       2                       3                       4......5......
+     for j in range(6):    #0,1 0,2 0,3 0,4 0,5       1,0 1,2 1,3 1,4 1,5     2,0 2,2 2,3 2,4 2,5     3,0 3,2 3,3 3,4 3,5
+          print(i,j)       #
 
 #4. print when the sum is odd
 
@@ -28,4 +35,12 @@ for i in range(1,11):
         for j in range(1,11):
                 x=i+j
                 if x%2!=0:
+                        print(i,j)
+
+#5. print when the sum is even
+
+for i in range(1,11):
+        for j in range(1,11):
+                x=i+j
+                if x%2==0:
                         print(i,j)
