475ed8430067a588883210ea795d318763371d88
SURA TEJASWANI <sura.tejaswani.5@gmail.com>
2025-06-17 19:16:36 +0530

    Create FUNCTION.py

---
+++ FUNCTION.py
@@ -0,0 +1,85 @@
+#def hel():     # hel
+#     v=10       #10
+#     b=200      #200
+#     c=v+b      #210
+#     return c   #210
+
+# hel()           # no output as not printing anything just storing value in hel()
+
+# store avutadi kani print kadu as the fun get destorys after return
+
+# def hello():                    #hello - 1
+#     for i in range(1,10):       # i=1
+#         return i                # 1  loop gets break as return was given and it will only print 1 value
+
+# print(hello())                  # hello - 2
+
+
+# def hello():            # hello
+#     v=10                # 10
+#     h=20                # 20
+#     return h            # 20  ==> output
+#     return v            # loop gets break
+
+# print(hello())          # hello
+
+
+# def hello():                          # hello
+#     v=10                              # 10
+#     return v                          # 10
+
+# xyz=hello()                           # 10
+
+# xyz="even" if xyz%2==0 else "odd"     # true 
+# print(xyz)                            # even
+
+# def xyz(v,y):
+#     return v-y
+
+# i=xyz(10,20)
+
+# i="positive" if i<0 else "negative"
+# print(i)
+
+
+
+# def sum_range(n):
+#     sum=0
+#     for i in range(n+1):
+#         sum+=i
+#     return sum
+
+# b=sum_range(10)
+
+# print(b)
+
+
+
+# positional argument example:-----
+
+# def sum_range(n,p):
+#     sum=0
+#     for i in range(n, p+1):
+#         sum+=i
+#     return sum
+
+# b=sum_range(1,10)
+
+# print(b)
+
+# if b<=150:
+#     print("value is lesser")
+# else:
+#     print("value is greater")
+
+
+# Kaprekar Number: Check whether a number is a Kaprekar number or not (you can define the logic and challenge students).
+
+# Digit Frequency Counter: Given a number, print the frequency of each digit (0-9).
+
+# Sum of Alternating Series: 1 - 2 + 3 - 4 + 5 - ... N terms
+
+# Nearest Prime to N: Given N, find the nearest prime number less than or equal to N.
+
+# Given N, print the first N Fibonacci numbers.
+# Print numbers between 1 to N, replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", and both with "FizzBuzz".
