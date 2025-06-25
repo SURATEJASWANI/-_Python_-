926c59e9f034b7dc4790f5584374c2d2aa59d360
SURA TEJASWANI <sura.tejaswani.5@gmail.com>
2025-06-25 17:15:01 +0530

    Create List comprehension.py

---
+++ List comprehension.py
@@ -0,0 +1,14 @@
+# 1.	Write a Python program that takes an amount n = 5200 and breaks it down into the minimum number of currency notes available in                         denominations of 500, 200, 100, 50, 20, 10, and 1 rupees.
+#Output: 10 500rupees notes and 1 200rupees notes
+
+# 2.	Write a Python program to keep asking the user to enter a password until they enter the correct
+
+
+# 3.	 Convert a 2D list into a flat 1D list using list comprehension.
+
+# 4.	Given a string, use list comprehension to create a list of all characters, excluding vowels.
+
+# 5.	From a given list of words, extract only those that start with a vowel (a, e, i, o, u).
+#Example:
+#words = ["functions", "loops", "oops", "exception", "as"]
+
