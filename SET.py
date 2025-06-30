792411d122985e8cd30c4496088d12f567c9bfd5
SURA TEJASWANI <sura.tejaswani.5@gmail.com>
2025-06-30 21:50:52 +0530

    Update SET.py

--- SET.py
+++ SET.py
@@ -5,3 +5,29 @@
 5. Write a Python program to find the product of all elements in a list.
 6. Write a Python program to check whether a given number is an Armstrong number or not. EX: K=153
 7. Write a Python program to check whether a given number is a Perfect number or not. Perfect number: sum of all factors of a given number
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+
+1.	Create a set with elements from 1 to 5. Add elements 6 and 7 to the set in one line.
+
+2.	Given two sets:
+           A = {1, 2, 3, 4, 5}
+           B = {4, 5, 6, 7, 8}
+Find elements that are present in A or B but not both (symmetric difference).
+
+3.	Remove an element from a set, but avoid error if element doesn't exist.
+ Find maximum and minimum element from a set {5, 8, 12, 3, 15, 7}.
+
+4.	Create a set with the values: 10, 20, 30, 40. Then add the value 50 to the set.
+
+5.	Remove an element 30 from a set {10, 20, 30, 40, 50} using a set method.
+
+6.	Check whether the number 25 exists in the set {15, 20, 25, 30, 35}.
+
+7.	Find the union of two sets:
+           A = {1, 2, 3, 4}
+           B = {3, 4, 5, 6}
+
+8.	Find the intersection of two sets:
+           A = {10, 20, 30, 40}
+            B = {30, 40, 50, 60}
