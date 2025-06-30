7be71bd776d632d5709a25fc66406ea72530952a
SURA TEJASWANI <sura.tejaswani.5@gmail.com>
2025-06-30 21:49:13 +0530

    Create SET.py

---
+++ SET.py
@@ -0,0 +1,7 @@
+1. Write a Python program to rotate a list to the right by k steps.
+2. Write a Python program to find the sum of prime digits in the number L = 123456.
+3. Write a Python program to find the sum of even digits in the number M = 65897.
+4. Write a Python program to calculate the sum of the largest elements from each row of a 2D list. EX: M=[[1,2,3],[4,5,6],[7,8,9]] output: 24
+5. Write a Python program to find the product of all elements in a list.
+6. Write a Python program to check whether a given number is an Armstrong number or not. EX: K=153
+7. Write a Python program to check whether a given number is a Perfect number or not. Perfect number: sum of all factors of a given number
