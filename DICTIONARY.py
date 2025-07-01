af0e080e76a2a20c9dddb7e7af9ebc5a90eca48b
SURA TEJASWANI <sura.tejaswani.5@gmail.com>
2025-07-01 18:01:51 +0530

    Create DICTIONARY.py

---
+++ DICTIONARY.py
@@ -0,0 +1,14 @@
+1.	Create a dictionary from two lists:
+a.	keys = ['id', 'name', 'age']
+b.	values = [101, 'John', 25]
+
+2.	Create a dictionary to store student name and age.
+
+3.	Create an empty dictionary and add key-value pairs one by one.
+
+4.	Get the value of key "salary" from this dictionary:
+                    EX: employee = {'name': 'John', 'age': 30, 'salary': 50000}
+
+5.	 Remove the last inserted key-value pair from the dictionary using an appropriate method
+
+6.	Define packing and unpacking in Python. Also, provide one example for both packing and unpacking.
